fba04060c34773abda9242ee96c942eff4d8db78
Krasnov1234 <62741165+Krasnov1234@users.noreply.github.com>
2021-06-29 00:32:54 +0700

    Добавлены данные

--- Проект структуры/Program.cs
+++ Проект структуры/Program.cs
@@ -10,6 +10,19 @@ namespace Проект_структуры
     {
         static void Main(string[] args)
         {
+            Random ran = new Random();
+            int x = 4;
+            int y = 4;
+            int[,] ara = new int[x, y];
+            for (int i = 0; i <= x - 1; i++)
+            {
+                for (int j = 0; j <= y - 1; j++)
+                {
+                    ara[i, j] = ran.Next(0, 10);
+                    Console.Write(ara[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
